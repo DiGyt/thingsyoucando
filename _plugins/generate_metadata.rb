91192013a0d7931601c1f4d719d1a0f838dacb4a
Dirk Gütlin <34550289+DiGyt@users.noreply.github.com>
2025-10-12 15:46:50 +0200

    Dynamically search the posts for tags rather than hardcode

--- _plugins/generate_metadata.rb
+++ _plugins/generate_metadata.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'fileutils'
 
 Jekyll::Hooks.register :site, :post_write do |site|
   things = site.collections['things'].docs.map do |doc|
@@ -13,8 +14,27 @@ Jekyll::Hooks.register :site, :post_write do |site|
     }
   end
 
-  FileUtils.mkdir_p(File.join(site.dest, 'assets', 'data'))
-  File.open(File.join(site.dest, 'assets', 'data', 'things-metadata.json'), 'w') do |f|
+  # Create the metadata JSON for items
+  data_dir = File.join(site.dest, 'assets', 'data')
+  FileUtils.mkdir_p(data_dir)
+  File.open(File.join(data_dir, 'things-metadata.json'), 'w') do |f|
     f.write(JSON.pretty_generate(things))
+  end
+
+  # Generate dynamic filters
+  all_categories = things.flat_map { |t| t[:categories] }.uniq.sort
+  all_countries  = things.map { |t| t[:country] }.uniq.sort
+  all_durations  = things.map { |t| t[:duration] }.uniq.sort
+  all_online     = [true, false]  # You can optionally filter based on which values exist
+
+  filters = {
+    'categories' => all_categories,
+    'country'    => all_countries,
+    'duration'   => all_durations,
+    'online'     => all_online
+  }
+
+  File.open(File.join(data_dir, 'things-filters.json'), 'w') do |f|
+    f.write(JSON.pretty_generate(filters))
   end
 end

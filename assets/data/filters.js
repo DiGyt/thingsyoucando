a375530c72421e2d246ffae8c03607920e0dd6e3
DiGyt <DiGyt@users.noreply.github.com>
2025-10-20 20:44:38 +0000

    deploy: 9b51d27b50f262243c52db8460e5a543472415d8

--- assets/data/filters.js
+++ assets/data/filters.js
@@ -1,19 +1,22 @@
 const filtersData = {
   categories: [
   "Community",
+  "Donations",
   "Ecology",
   "Movement",
   "Volunteering"
 ],
   country: [
   "France",
+  "Germany",
   "Spain",
   "USA"
 ],
   duration: [
   "15 minutes",
   "2 hours",
-  "30 minutes"
+  "30 minutes",
+  "5 minutes"
 ],
   online: [
   true,
